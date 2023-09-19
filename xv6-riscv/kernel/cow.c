97e1e8ad5373771c3b89f862238b9a93a8c6cf14
rahulpriolkar <rahulpriolkar01@gmail.com>
2023-09-18 19:45:05 -0700

    lab2 added

---
+++ xv6-riscv/kernel/cow.c
@@ -0,0 +1,113 @@
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "riscv.h"
+#include "spinlock.h"
+#include "proc.h"
+#include "defs.h"
+#include "elf.h"
+#include <stdbool.h>
+
+struct spinlock cow_lock;
+
+// Max number of pages a CoW group of processes can share
+#define SHMEM_MAX 100
+
+struct cow_group {
+    int group; // group id
+    uint64 shmem[SHMEM_MAX]; // list of pages a CoW group share
+    int count; // Number of active processes
+};
+
+struct cow_group cow_group[NPROC];
+
+struct cow_group* get_cow_group(int group) {
+    if(group == -1)
+        return 0;
+
+    for(int i = 0; i < NPROC; i++) {
+        if(cow_group[i].group == group)
+            return &cow_group[i];
+    }
+    return 0;
+}
+
+void cow_group_init(int groupno) {
+    for(int i = 0; i < NPROC; i++) {
+        if(cow_group[i].group == -1) {
+            cow_group[i].group = groupno;
+            return;
+        }
+    }
+} 
+
+int get_cow_group_count(int group) {
+    return get_cow_group(group)->count;
+}
+void incr_cow_group_count(int group) {
+    get_cow_group(group)->count = get_cow_group_count(group)+1;
+}
+void decr_cow_group_count(int group) {
+    get_cow_group(group)->count = get_cow_group_count(group)-1;
+}
+
+void add_shmem(int group, uint64 pa) {
+    if(group == -1)
+        return;
+
+    uint64 *shmem = get_cow_group(group)->shmem;
+    int index;
+    for(index = 0; index < SHMEM_MAX; index++) {
+        // duplicate address
+        if(shmem[index] == pa)
+            return;
+        if(shmem[index] == 0)
+            break;
+    }
+    shmem[index] = pa;
+}
+
+int is_shmem(int group, uint64 pa) {
+    if(group == -1)
+        return 0;
+
+    uint64 *shmem = get_cow_group(group)->shmem;
+    for(int i = 0; i < SHMEM_MAX; i++) {
+        if(shmem[i] == 0)
+            return 0;
+        if(shmem[i] == pa)
+            return 1;
+    }
+    return 0;
+}
+
+void cow_init() {
+    for(int i = 0; i < NPROC; i++) {
+        cow_group[i].count = 0;
+        cow_group[i].group = -1;
+        for(int j = 0; j < SHMEM_MAX; j++)
+            cow_group[i].shmem[j] = 0;
+    }
+    initlock(&cow_lock, "cow_lock");
+}
+
+int uvmcopy_cow(pagetable_t old, pagetable_t new, uint64 sz) {
+    
+    /* CSE 536: (2.6.1) Handling Copy-on-write fork() */
+
+    // Copy user vitual memory from old(parent) to new(child) process
+
+    // Map pages as Read-Only in both the processes
+
+    return 0;
+}
+
+void copy_on_write() {
+    /* CSE 536: (2.6.2) Handling Copy-on-write */
+
+    // Allocate a new page 
+    
+    // Copy contents from the shared page to the new page
+
+    // Map the new page in the faulting process's page table with write permissions
+}
